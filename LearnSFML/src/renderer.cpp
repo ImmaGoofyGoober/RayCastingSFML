927fddcfd443e1ca480f6772527ed84398b48c4b
ImmaGoofyGoober <107286583+ImmaGoofyGoober@users.noreply.github.com>
2026-06-09 01:17:16 -0400

    remove comment

--- LearnSFML/src/renderer.cpp
+++ LearnSFML/src/renderer.cpp
@@ -23,7 +23,7 @@
 
 RayCaster Renderer::InitializeScene() {
     // RayCaster
-    RayCaster rayCaster(25.f, 400.f, 4000, {700.f, 300.f}, sf::Color::Yellow, sf::Color::Yellow); // raySourceRadius, rayLength, position, color
+    RayCaster rayCaster(25.f, 400.f, 4000, {700.f, 300.f}, sf::Color::Yellow, sf::Color::Yellow); 
 
     // Circle
     sceneObjects_.push_back(std::make_unique<Circle>(30.f, sf::Vector2f{500.f,500.f}, sf::Color::Blue, 180.f, 1.f, 0.6f, false));
